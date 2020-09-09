b00ad6e77be9092c9a62c94326bd1bd53a89b270
永格天 <we684123@gmail.com>
2020-09-09 21:33:53 +0800

    加個註解

--- config/base.py
+++ config/base.py
@@ -1,11 +1,12 @@
 def base():
     return {
-        "login_email": "xxx@gmail.com",
-        "application_password": "oxoxoxoxoxoxo", # 請去跟google拿
-        "header": "發送 inline image",
-        "from_email": "xxx@gmail.com",
+        "login_email": "xxx@gmail.com",  # 要登入的email
+        "application_password": "oxoxoxoxoxoxo",  # 請去跟google拿(詳細看Readme)
+        "header": "發送 inline image",  # email標題
+        "from_email": "xxx@gmail.com",  # 寄送者email
+        # 以陣列塞入要發送的email
         "to_emails": ["xxx@gmail.com", "ooo@gm.lhu.edu.tw"],
-        "images": ['1.png', '2.png', '3.png'],
+        "images": ['1.png', '2.png', '3.png'],  # 要塞入的圖片
         "html_file": "template.html",
         "logging_level": "DEBUG",  # DEBUG #INFO #ERROR
         "log_file_path": './logs/send_mail_log',
