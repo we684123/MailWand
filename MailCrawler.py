3718bab026f64785d4b83d08dc98b85eea1cbca0
we684123 <we684123@gmail.com>
2020-10-07 14:29:18 +0800

    ✨ 將 MaiCrawler 改成可指定參數

--- MailCrawler.py
+++ MailCrawler.py
@@ -14,47 +14,68 @@ from get_filename import get_filename
 from config import base
 base = base.base()
 
-# 載入設定
-login_email = base['login_email']
-application_password = base['application_password']
-
-header = base['header']
-from_email = base['from_email']
-images_path = base['images_path']
-html_file = base['html_file']
-
-logging_level = base['logging_level']
-log_file_path = base['log_file_path']
-log_format = base['log_format']
 # ======設定完畢=====
 
-logger = logging.getLogger(__name__)
-handler1 = logging.StreamHandler(sys.stdout)
-handler2 = logging.handlers.TimedRotatingFileHandler(
-    filename=log_file_path,
-    when='D',
-    encoding='utf-8'
-)
-formatter = logging.Formatter(log_format)
-handler1.setFormatter(formatter)
-handler2.setFormatter(formatter)
-
-logger.setLevel(logging_level)
-handler1.setLevel(logging_level)
-handler2.setLevel(logging_level)
-
-logger.addHandler(handler1)
-logger.addHandler(handler2)
-
-coloredlogs.install(level=logging_level, logger=logger)
-# logger.info("基礎設定已載入.")
-# logger設定完畢
 
 
 class MailCrawler():
     """生產線般的寄出 mail."""
 
-    def __init__(self, variable={}):
+    def __init__(self, variable={},**kwargs):
+        # base載入設定
+        self.login_email = base['login_email']
+        if 'login_email' in kwargs :
+            self.login_email = kwargs['login_email']
+        self.application_password = base['application_password']
+        if 'application_password' in kwargs :
+            self.application_password = kwargs['application_password']
+
+        self.header = base['header']
+        if 'header' in kwargs :
+            self.header = kwargs['header']
+        self.from_email = base['from_email']
+        if 'from_email' in kwargs :
+            self.from_email = kwargs['from_email']
+        self.images_path = base['images_path']
+        if 'images_path' in kwargs :
+            self.images_path = kwargs['images_path']
+        self.html_file = base['html_file']
+        if 'html_file' in kwargs :
+            self.html_file = kwargs['html_file']
+
+        self.logging_level = base['logging_level']
+        if 'logging_level' in kwargs :
+            self.logging_level = kwargs['logging_level']
+        self.log_file_path = base['log_file_path']
+        if 'log_file_path' in kwargs :
+            self.log_file_path = kwargs['log_file_path']
+        self.log_format = base['log_format']
+        if 'log_format' in kwargs :
+            self.log_format = kwargs['log_format']
+
+
+        logger = logging.getLogger(__name__)
+        handler1 = logging.StreamHandler(sys.stdout)
+        handler2 = logging.handlers.TimedRotatingFileHandler(
+            filename=self.log_file_path,
+            when='D',
+            encoding='utf-8'
+        )
+        formatter = logging.Formatter(self.log_format)
+        handler1.setFormatter(formatter)
+        handler2.setFormatter(formatter)
+
+        logger.setLevel(self.logging_level)
+        handler1.setLevel(self.logging_level)
+        handler2.setLevel(self.logging_level)
+
+        logger.addHandler(handler1)
+        logger.addHandler(handler2)
+
+        coloredlogs.install(level=self.logging_level, logger=logger)
+        # logger.info("基礎設定已載入.")
+        # logger設定完畢
+
         self._load_html(variable)
         self._generate_mail()
         self._load_smtp()
@@ -62,7 +83,7 @@ class MailCrawler():
         logger.info('==== All is ready====')
 
     def _load_html(self, variable):
-        with open(html_file, mode='r', encoding='utf-8') as file:
+        with open(self.html_file, mode='r', encoding='utf-8') as file:
             html = file.read()
 
         for key in variable.keys():
@@ -76,7 +97,7 @@ class MailCrawler():
         msg.attach(MIMEText(self.html, 'html', 'utf-8'))
         logger.info("email content generated successfully.")
 
-        image_list = list(Path(images_path).glob('*.png'))
+        image_list = list(Path(self.images_path).glob('*.png'))
         for image in image_list:
             image_name = image.name
             image_filename = get_filename(image.name, 'filename')
@@ -84,7 +105,7 @@ class MailCrawler():
             pic = MIMEBase('image', image_extension)
             pic.add_header(
                 'Content-ID', '<{0}>'.format(image_filename))
-            with open(images_path + image.name, 'rb') as f:
+            with open(self.images_path + image.name, 'rb') as f:
                 pic.set_payload(f.read())
             encoders.encode_base64(pic)
             msg.attach(pic)
@@ -93,24 +114,24 @@ class MailCrawler():
 
     def _load_smtp(self):
         smtp = smtplib.SMTP_SSL('smtp.gmail.com')
-        smtp.login(login_email, application_password)
-        logger.info("Gmail {0} logined.".format(login_email))
+        smtp.login(self.login_email, self.application_password)
+        logger.info("Gmail {0} logined.".format(self.login_email))
         self.smtp = smtp
 
     def send_to_mail(self, to_emails):
         logger.info("====== Start sending! =====")
         for to_email in to_emails:
-            self.msg['From'] = from_email
+            self.msg['From'] = self.from_email
             self.msg['To'] = to_email
-            self.msg['Subject'] = Header(header, 'utf-8').encode()
+            self.msg['Subject'] = Header(self.header, 'utf-8').encode()
             status = self.smtp.sendmail(
-                from_email, to_email, self.msg.as_string())
+                self.from_email, to_email, self.msg.as_string())
             if status == {}:
                 logger.info("{0} {1} Mail sent successfully!✅".format(
-                    to_email, header))
+                    to_email, self.header))
             else:
                 logger.error("{0} {1} Mail sent failed!🚨".format(
-                    to_email, header))
+                    to_email, self.header))
                 logger.error(status)
         logger.info('====== All sent ======')
 
