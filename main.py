bf5d022592c0381de5b52aaf9a2399567d0b910c
we684123 <we684123@gmail.com>
2020-10-07 14:42:48 +0800

    📝更新 main 的示範

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ if __name__ == '__main__':
 
     # variable is 'Optional parameters'
     variable = {'{Sir}': 'people_name'}
-    MailCrawler = MailCrawler(variable)
-
+    MailCrawler_1 = MailCrawler(variable)
     to_emails = ['xxx@gmail.com', 'ooo@gmail.com']
-    MailCrawler.send_to_mail(to_emails)
-    MailCrawler.close()
+    MailCrawler_1.send_to_mail(to_emails)
+    MailCrawler_1.close()
+
+    # use specify parameters
+    variable2 = {'{Sir}': 'people_name2'}
+    MailCrawler2 = MailCrawler(
+        variable2,
+        header='這是第2種',
+        images_path='./images2/',
+        html_file='./template2.html'
+    )
+    to_emails2 = ['xxx@gmail.com', 'ooo@gmail.com']
+    MailCrawler2.send_to_mail(to_emails2)
+    MailCrawler2.close()
