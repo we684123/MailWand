a90b2b75c447988f9bc157c751ffe1f917f3a39d
永格天 <we684123@gmail.com>
2020-10-31 00:37:20 +0800

    ✏️ base.py

--- config/base.py
+++ config/base.py
@@ -5,7 +5,7 @@ def base():
         "header": "發送 embed image",  # email標題
         "from_email": "ooo@gmail.com",  # 寄送者email
         "images_path":"./images/", # 目前圖片只支援png
-        "attachments_path":"./attachments/" # 要隨信附上的附件的資料夾位置
+        "attachments_path":"./attachments/", # 要隨信附上的附件的資料夾位置
         "html_file": "template.html",
         "logging_level": "DEBUG",  # DEBUG #INFO #ERROR
         "log_file_path": './logs/send_mail_log',
