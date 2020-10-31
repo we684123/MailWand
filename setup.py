38733d9b0b5082aa87ab9fa9e837977bc4faeb65
永格天 <we684123@gmail.com>
2020-10-31 14:12:01 +0800

    ➕ coloredlogs

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     platforms="any",
-    install_requires=['get_filename']
+    install_requires=['get_filename','coloredlogs']
 )
