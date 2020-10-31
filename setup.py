d39fa4a992dd99da788472d0ae68e57067e2eaa2
永格天 <we684123@gmail.com>
2020-10-31 14:01:33 +0800

    📦 v1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name="MailCrawler",
-    version="1.0.0",
+    version="1.0.1",
     keywords=("mail", "email","crawler","embed","image"),
     description="link to crawler send email",
     long_description=long_description,
