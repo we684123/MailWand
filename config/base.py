4d160ea77b48564707625a0d8f766450c073777b
永格天 <we684123@gmail.com>
2020-10-31 12:40:15 +0800

    📝 加個註解

--- config/base.py
+++ config/base.py
@@ -6,7 +6,7 @@ def base():
         "from_email": "ooo@gmail.com",  # 寄送者email
         "images_path":"./images/", # 目前圖片只支援png
         "attachments_path":"./attachments/", # 要隨信附上的附件的資料夾位置
-        "html_file": "template.html",
+        "html_file": "template.html", # html 檔案的位置
         "logging_level": "DEBUG",  # DEBUG #INFO #ERROR
         "log_file_path": './logs/send_mail_log',
         "log_format": '%(asctime)s - %(levelname)s : %(message)s',
